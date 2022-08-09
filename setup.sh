c1835879310da944bff371a42a06c6aeae221a58
dreamsproxy <76526328+dreamsproxy@users.noreply.github.com>
2022-08-09 20:21:14 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo apt install hostapd iproute2 iwconfig util-linux procps
-sudo apt install tshark
+sudo apt install hostapd iproute2 iw util-linux procps wireshark
 git clone https://github.com/oblique/create_ap
 cd create_ap
 make install
