4f4ab99ec32e639f8b74e471d7f3c5312ff0f243
dreamsproxy <76526328+dreamsproxy@users.noreply.github.com>
2022-08-12 15:20:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def start_rogueAP(wlan_id, ethernet_id):
     ws_filters = str('"http"')
     ap_command = str(f"x-terminal-emulator -e 'sudo create_ap --no-virt -m nat {wlan_id} {ethernet_id} TPE-Free'")
     ws_command = str(f"x-terminal-emulator -e 'sudo wireshark -k -i {wlan_id} -Y {ws_filters} '")
-    
+    #print(wlan_id, ethernet_id)
     subprocess.Popen(shlex.split(ap_command))
     #    Wait 10 seconds for AP to be fully initialized
     time.sleep(10)
@@ -17,8 +17,8 @@ def start_rogueAP(wlan_id, ethernet_id):
 
 def main():
     interfaces = search_ifaces()
-    ethernet = interfaces[0]
-    wlan = interfaces[1]
+    ethernet = interfaces["ethernet"]
+    wlan = interfaces["wireless"]
     start_rogueAP(wlan, ethernet)
 
 if __name__ == "__main__":
